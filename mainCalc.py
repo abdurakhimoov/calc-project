2b63a8fcdcc776bb5ab6e845574c2ea3338c4897
Abduqodir <abdurahimovabduqdir96@gmail.com>
2025-07-29 19:47:05 +0500

    added mini calc

---
+++ mainCalc.py
@@ -0,0 +1,22 @@
+class Calc:
+    def __init__(self,a,b,func):
+        self.a = a
+        self.b = b
+        self.func = func
+
+    def add(self):
+        if self.func == "+":
+            return self.a + self.b 
+        elif self.func == "-":
+            return self.a - self.b
+        elif self.func == "*":
+            return self.a * self.b
+        elif self.func == "/":
+            return self.a // self.b
+
+a = int(input("a-son kiriting: "))
+b = int(input("b-son kiriting: "))
+ff = input("operator... : ")
+
+full = Calc(a,b, ff)
+print(f"Javob: {full.add()}")
